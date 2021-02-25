8056399e0ae091cc45835579ba991c68d8c49656
sahidulcse12 <m.sahidulislam.14@gmail.com>
2021-02-25 16:26:40 +0600

    another try

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [nayoks, setNayoks] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setNayoks(data))
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          nayoks.map(nayok => <Nayok name={nayok.name} address={nayok.address}></Nayok>)
+        }
+
       </header>
     </div>
   );
+}
+
+function Nayok(props) {
+  console.log(props)
+  // const values = Object.values(props);
+
+  const styleDiv = {
+    border: '5px solid white',
+    borderRadius: '10px',
+    margin: '20px',
+    padding: '20px'
+    
+  }
+  return (
+      <div style={styleDiv}>
+        <h2>Name : {props.name}</h2>
+        <h2>Address : {props.address.city}</h2>
+      </div>
+
+  )
 }
 
 export default App;
